9e76f144119f0b67a85c0bcd568f1031473083cd
Justin Patterson <jhpatt33@gmail.com>
2021-03-04 16:34:27 -0500

    Lab 4

---
+++ Assets/Scripts/Gameplay/GameManager.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public enum GamePhases
+    {
+        StartPhase, PlayPhase
+    }
+
+    public GamePhases currentGamePhase = GamePhases.StartPhase;
+
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+
+            StartCurrentPhaseBehavior();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void SetNextPhase(GamePhases nextPhase)
+    {
+        EndCurrentPhaseBehavior();
+        currentGamePhase = nextPhase;
+        StartCurrentPhaseBehavior();
+    }
+
+    void StartCurrentPhaseBehavior()
+    {
+        switch (currentGamePhase)
+        {
+            case GamePhases.StartPhase:
+                //load data
+                //get microtransactions
+
+                if(SceneManager.GetActiveScene().buildIndex != 0)
+                    SceneManager.LoadScene(0);
+
+                OnUnPaused();
+                break;
+            case GamePhases.PlayPhase:
+                SceneManager.LoadScene(1);
+
+                OnUnPaused();
+                break;
+        }
+    }
+    void EndCurrentPhaseBehavior()
+    {
+        switch (currentGamePhase)
+        {
+            case GamePhases.StartPhase:
+                break;
+            case GamePhases.PlayPhase:
+                break;
+        }
+    }
+
+    public void OnStartGamePressed ()
+    {
+        SetNextPhase(GamePhases.PlayPhase);
+    }
+
+    public void OnPlayQuitPressed()
+    {
+        SetNextPhase(GamePhases.StartPhase);
+    }
+
+    public void OnPaused()
+    {
+        Time.timeScale = 0f;
+    }
+    public void OnUnPaused()
+    {
+        Time.timeScale = 1f;
+    }
+}
--- Assets/Scripts/UI/MenuController.cs
+++ Assets/Scripts/UI/MenuController.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class MenuController : MonoBehaviour
 {
+    public void OnPlayQuitPressed()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.OnPlayQuitPressed();
+        }
+    }
+    public void OnStartGamePressed()
+    {
+        if (GameManager.instance != null)
+        {
+            GameManager.instance.OnStartGamePressed();
+        }
+    }
 
+    public void TriggerPause(bool isPaused)
+    {
+        if (GameManager.instance != null)
+        {
+            if (isPaused)
+            { GameManager.instance.OnPaused(); }
+            else
+            { GameManager.instance.OnUnPaused(); }
+        }
+    }
 }
